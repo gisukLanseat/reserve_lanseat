47b47e5c70ff036397fd6b5bbd30136ba52de8e5
bdmg114 <65209655+bdmg114@users.noreply.github.com>
2023-08-20 22:18:10 +0900

    fsc

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from .models import reservation
 from django.utils import timezone
 from . import seat
 from django.http import Http404
+from datetime import timedelta
 
 # Create your views here.
 def index(request):
@@ -15,11 +16,14 @@ def index(request):
                 error_msg.append("방 번호와 이름을 입력하세요")
             context = {'seats':seat.seats.keys, "error_msg":error_msg}
             return render(request, "main/main.html", context)
-    context = {'seats':seat.seats.keys}
+    context = {'seats':seat.seats.keys, 'time':timezone.localtime().hour}
     return render(request, "main/main.html", context)
 
 def get_reserved(room_id):
-    r = reservation.objects.filter(date=timezone.localtime().date())
+    if timezone.localtime().hour < 2:
+        r = reservation.objects.filter(date=timezone.localtime().date()-timedelta(1))
+    else:
+        r = reservation.objects.filter(date=timezone.localtime().date())
     reserved = []
     ids = []
     for a in seat.seats[room_id]:
@@ -42,17 +46,25 @@ def start(request, number, room_id):
     
     reserved = get_reserved(room_id)
     print(reserved)
-    context = {'student':str(number), 'seats':seat.seats[room_id], 'reserved':reserved[0], "room":room_id}
+    context = {'student':str(number), 'seats':seat.seats[room_id], 'reserved':reserved[0], "room":room_id, 'time':timezone.localtime().hour}
     return render(request, "main/reserve.html", context)
 
 def reserve(request, number, room_id):
     if request.method == "POST":
-        if not reservation.objects.filter(student=number).filter(date=timezone.localtime().date()) and not reservation.objects.filter(seat=request.POST.get("seat")).filter(date=timezone.localtime().date()):
-            r = reservation(seat=request.POST.get("seat"), date=timezone.localtime().date(), student=number)
-            print(timezone.localtime())
-            r.save()
+        if timezone.localtime().hour >= 9:
+            if not reservation.objects.filter(student=number).filter(date=timezone.localtime().date()) and not reservation.objects.filter(seat=request.POST.get("seat")).filter(date=timezone.localtime().date()):
+                r = reservation(seat=request.POST.get("seat"), date=timezone.localtime().date(), student=number)
+                print(timezone.localtime())
+                r.save()
+        elif timezone.localtime().hour < 2:
+            if not reservation.objects.filter(student=number).filter(date=timezone.localtime().date()-timedelta(1)) and not reservation.objects.filter(seat=request.POST.get("seat")).filter(date=timezone.localtime().date()-timedelta(1)):
+                r = reservation(seat=request.POST.get("seat"), date=timezone.localtime().date()-timedelta(1), student=number)
+                print(timezone.localtime())
+                r.save()
         else:
             error_msg = []
+            if 1 < timezone.localtime().hour and timezone.localtime().hour < 9:
+                error_msg.append("예약 불가능 시간입니다(예약 가능 시간:오전 9시~익일 오전 1시)")
             if reservation.objects.filter(student=number).filter(date=timezone.localtime().date()):
                 instance = reservation.objects.filter(student=number).filter(date=timezone.localtime().date()).filter(seat=request.POST.get("seat"))
                 if instance:
@@ -72,7 +84,10 @@ def dashBoard(request):
             return redirect("common:login")
     else:
         return redirect("common:login")
-    r = reservation.objects.filter(date=timezone.localtime().date())
+    if timezone.localtime().hour < 2:
+        r = reservation.objects.filter(date=timezone.localtime().date()-timedelta(1))
+    else:
+        r = reservation.objects.filter(date=timezone.localtime().date())
     reserved = {}
     ids = {}
     for x in seat.seats:
