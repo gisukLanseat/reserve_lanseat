663b6eea34864bc78d58b401be0fe94001d0c1f2
bdmg114 <65209655+bdmg114@users.noreply.github.com>
2024-01-03 19:55:27 +0900

    ddd

--- config/settings/base.py
+++ config/settings/base.py
@@ -121,8 +121,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_DIR = os.path.dirname(BASE_DIR)
 
 # static files
