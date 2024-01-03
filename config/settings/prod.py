6120bbd47d3a2bc9dd00ebc34380a6e96bb2f443
bdmg114 <65209655+bdmg114@users.noreply.github.com>
2024-01-03 19:36:51 +0900

    ddd

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['13.124.205.11', 'gghslanseat.site']
+ALLOWED_HOSTS = ['222.122.81.136', 'gghslanseat.site']
 STATIC_ROOT = BASE_DIR / 'static/'
 
 STATICFILES_DIRS = []
