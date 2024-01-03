132f648b139e8816d53d8d69b84b91403fae14dc
bdmg114 <65209655+bdmg114@users.noreply.github.com>
2024-01-03 19:53:37 +0900

    ddf

--- config/settings/base.py
+++ config/settings/base.py
@@ -119,8 +119,16 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+import os
 
-LOGIN_REDIRECT_URL = '/dash/'
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+
+# static files
+STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
