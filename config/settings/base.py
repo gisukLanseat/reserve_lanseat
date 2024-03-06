b54acb53542bb09d81ee828ee71b4d00798ceec1
bdmg114 <65209655+bdmg114@users.noreply.github.com>
2024-03-06 13:23:58 +0900

    login_fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+LOGIN_REDIRECT_URL = '/dash'
 
 import os
 
--- main/urls.py
+++ main/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('cancel/<str:id>', view=views.cancel, name="cancel"),
     path('<str:number>/<str:room_id>', views.start, name='start'),
     path('reserve/<str:room_id>/<str:number>', view=views.reserve, name="reserve"),
-    path('dash/', view=dashBoard, name="dashboard")
+    path('dash/', view=dashBoard, name="dashboard"),
+    path('api/', views.api_index, name="api_main"),
+    path('api/<str:number>/<str:room_id>', views.api_start, name='api_start'),
+    path('api/reserve/<str:room_id>/<str:number>', view=views.api_reserve, name="api_reserve"),
 ]
